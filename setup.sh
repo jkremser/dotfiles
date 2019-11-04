90ce69421397bb02557dbbeb2da4054f8342ee4a
Jirka Kremser <jkremser@redhat.com>
2019-11-04 21:00:12 +0100

    keyboard settings in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 20
+gsettings set org.gnome.desktop.peripherals.keyboard delay 300
+
 for i in $(ls -A); do
   [ "README" != $i ] && [ ".git" != $i ] && [[ $0 != *$i ]] &&  {
     echo copying $i to ~
