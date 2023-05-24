232aff702d65c46d9fcfa0afefd5330d8139ba74
Jirka Kremser <jiri.kremser@gmail.com>
2023-05-24 12:41:44 +0200

    capv aliases
    Signed-off-by: Jirka Kremser <jiri.kremser@gmail.com>

--- .functions/.giantswarm.sh
+++ .functions/.giantswarm.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 command -v opsctl &> /dev/null && source <(opsctl completion zsh)
 
@@ -93,6 +93,11 @@ function k_remove(){
   rm ~/.kube/config
 }
 
+# CAPV
+
+alias capvlog='k logs -f -l cluster.x-k8s.io/provider=infrastructure-vsphere -n giantswarm --tail=-1 | sed -e "s;^E.*;$(tput setaf 1)&$(tput sgr0);g"'
+alias capvlogErr='k logs -f -l cluster.x-k8s.io/provider=infrastructure-vsphere -n giantswarm --tail=-1 | grep "^E"'
+
 govc_delete_vms(){
   local _vms=$(govc ls /Datacenter/vm/ | fzf -m --header "Select VM(s) to be deleted by Shift+Tab")
   echo "Are you sure to delete following vms:"
@@ -109,6 +114,29 @@ copy_cfg() {
   ssh -o "StrictHostKeyChecking=no" -i ~/.ssh/giantswarm-sso-ed25519 root@${IP} -- "systemctl restart containerd && systemctl status containerd"
 }
 
+wait_for_svc() {
+  ip=""
+  echo "Waiting for external IP"
+  while [ -z $ip ]; do
+    printf "."
+    ip=$(kubectl get svc $@ --template="{{range .status.loadBalancer.ingress}}{{.ip}}{{end}}")
+    [ -z "$ip" ] && sleep 3
+  done
+  echo 'Found external IP: '$ip
+  say "service $1 has an external ip"
+}
+
+get_secret() {
+  [[ $# -lt 1 ]] && {
+		local _sec_and_ns=$(kubectl get secrets --no-headers -A | fzf --header "Select a secret" -e)
+		local _ns=$(echo $_sec_and_ns | awk '{print $1}')
+		local _sec=$(echo $_sec_and_ns | awk '{print $2}')
+    _cmd_args="$_sec -n $_ns"
+  } || _cmd_args=$@
+
+  kubectl get secret -o go-template='{{range $k,$v := .data}}{{printf "%s:\n" $k}}{{if not $v}}{{$v}}{{else}}{{$v | base64decode}}{{end}}{{"\n"}}{{end}}' $(echo ${_cmd_args})
+}
+
 get_wcs_kvm() {
   kubectl get kvmclusterconfigs -o yaml | yq '.items[].spec.guest.id'
 }
@@ -117,4 +145,4 @@ get_machines_in_installation(){
   opsctl show installation -i ${1} | yq -r '.Machines'
 }
 
-[[ -f ~/.giantswarm-personal.sh ]] && source ~/.giantswarm-personal.sh
+source ~/.giantswarm-personal.sh
